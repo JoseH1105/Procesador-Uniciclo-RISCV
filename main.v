a870711e991d12167b86a80d633e62153e621f38
pablohidalgoh <pabloandhidalgo@gmail.com>
2025-11-22 02:05:35 -0600

    Add files via upload

---
+++ main.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/22/2025 01:49:59 AM
+// Design Name: 
+// Module Name: main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main(
+
+    );
+endmodule
